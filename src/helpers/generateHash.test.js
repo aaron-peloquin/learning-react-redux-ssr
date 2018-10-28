8748a8cadab895dcc7d83e7b08a9c7684c1f25e0
Aaron <aaron4uk@gmail.com>
2018-10-28 00:13:10 -0500

    adding more tests

--- src/helpers/generateHash.test.js
+++ src/helpers/generateHash.test.js
@@ -3,3 +3,11 @@ import generateHash from './generateHash'
 test('generateHash() is a string', () => {
   expect(typeof generateHash()).toBe("string")
 })
+
+test('generateHash() length of 8', () => {
+  expect(generateHash().length).toBe(8)
+})
+
+test('generateHash() starts with an h', () => {
+  expect(generateHash()[0]).toBe("h")
+})
