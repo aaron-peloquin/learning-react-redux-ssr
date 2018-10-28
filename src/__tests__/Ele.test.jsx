67ad926b1fb729c99acce4583038e024011e0607
Aaron <aaron4uk@gmail.com>
2018-10-28 14:08:13 -0500

    moving Ele test into __tests__

--- src/__tests__/Ele.test.jsx
+++ src/__tests__/Ele.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import Ele from './Ele'
+import Ele from '../Ele'
 
 test('<Ele /> works', () => {
   const component = renderer.create(
