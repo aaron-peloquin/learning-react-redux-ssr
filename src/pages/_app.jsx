15149d7148a7a6fef2b19ccf39bc31446ba46b8b
Aaron <aaron4uk@gmail.com>
2018-11-03 00:50:04 -0500

    adding header bar to all pages

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,15 +2,16 @@ import App, {Container} from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
 
-import withReduxStore from './../store/AppWithRedux'
-
-import Navigation from '../components/Navigation'
+import withReduxStore from '../store/AppWithRedux'
+import SideBar from '../components/SideBar'
+import HeaderBar from './../components/HeaderBar'
 
 const MyApp = (props) => {
   const {Component, pageProps, router, reduxStore} = props
   return (
     <Container>
-      <Navigation />
+      <HeaderBar />
+      <SideBar />
       <Provider store={reduxStore}>
         <Component {...pageProps} router={router} />
       </Provider>
